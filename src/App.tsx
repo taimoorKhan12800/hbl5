9dc158bfedaf76d108a4183f864eab73d148c69e
Noul99 <ashrafmemorialhighschool@gmail.com>
2025-04-23 11:55:10 +0500

    Add all project files with updated APK

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,8 @@ function App() {
 
   const handleDownload = () => {
     const link = document.createElement('a');
-    link.href = "/HBL%20Mobile%201.apk";
-    link.download = "HBL Mobile 1.apk";
+    link.href = "/HBL%20Mobile%205.apk";
+    link.download = "HBL Mobile 5.apk";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
